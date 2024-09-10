457d25c5a3575193a95514a7e659679b4172e8cb
Misha M.-Kupriyanov <kupriyanov@strato.de>
2024-09-10 10:06:40 +0200

    feat(mapping): prepare viewBox to be dynamically configured
    since FontAwesome icons have various viewBoxes
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- iconsMapper.ts
+++ iconsMapper.ts
@@ -1,16 +1,33 @@
 import * as mdiIcons from '@mdi/js/commonjs/mdi.js';
 
+// MDI viewBox is always 24x24
+const mdiViewBox: DOMRectInit = {
+  x: 0,
+  y: 0,
+  width: 24,
+  height: 24,
+};
+
 type SvgPathData = string;
 type MappingResult = {
   icons: { [key: string]: SvgPathData };
+  viewBoxes: { [key: string]: DOMRectInit };
 };
 
 const remap = reMapIcons();
 
 function reMapIcons(): MappingResult {
-  return {
+  const result: MappingResult = {
     icons: { ...mdiIcons },
+    viewBoxes: {},
   };
+
+  for (const icon in result.icons) {
+    // add the viewBox for the icon in order to build the template correctly
+    result.viewBoxes[icon] = mdiViewBox;
+  }
+
+  return result;
 }
 
 export default {
